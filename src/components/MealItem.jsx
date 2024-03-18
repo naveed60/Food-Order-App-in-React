3ed9c94eae8b80352e6f0d3cb79fc4cc4b6e4c64
naveed60 <advmiannaveedali@gmail.com>
2024-03-18 15:00:40 +0500

    mealitem component added

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 
-function MealItem() {
+function MealItem({ meal }) {
   return (
-    <div>
-      
-    </div>
+
+    <li className='meal-item'>
+      <article>
+        <img src={`http://localhost:3000/${meal.image}`} alt={meal.name}/>
+        <div>
+          <h3>{meal.name}</h3>
+          <p className='meal-item-price'>{meal.price}</p>
+          <p className='meal-item-description'>{meal.description}</p>
+        </div>
+        <p className='meal-item-actions'>
+          <button>Add To Cart</button>
+        </p>
+      </article>
+
+    </li>
   )
 }
 
