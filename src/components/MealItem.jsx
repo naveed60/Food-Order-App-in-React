88e26bb9d9ec9b5c80f85bde0203d91a4e44b521
naveed60 <advmiannaveedali@gmail.com>
2024-03-21 16:31:12 +0500

    mealitem component updated

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
-import { currencyFormatter } from '../utils/formatting'
-import Button from './UI/Button'
+import { useContext } from 'react';
 
-function MealItem({ meal }) {
+import { currencyFormatter } from '../util/formatting.js';
+import Button from './UI/Button.jsx';
+import CartContext from '../store/CartContext.jsx';
+
+export default function MealItem({ meal }) {
+  const cartCtx = useContext(CartContext);
+
+  function handleAddMealToCart() {
+    cartCtx.addItem(meal);
+  }
+
   return (
-
-    <li className='meal-item'>
+    <li className="meal-item">
       <article>
-        <img src={`http://localhost:3000/${meal.image}`} alt={meal.name}/>
+        <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
-          <p className='meal-item-price'>
-            { currencyFormatter.format(meal.price)}</p>
-          <p className='meal-item-description'>{meal.description}</p>
+          <p className="meal-item-price">
+            {currencyFormatter.format(meal.price)}
+          </p>
+          <p className="meal-item-description">{meal.description}</p>
         </div>
-        <p className='meal-item-actions'>
-          <Button>Add To Cart</Button>
+        <p className="meal-item-actions">
+          <Button onClick={handleAddMealToCart}>Add to Cart</Button>
         </p>
       </article>
-
     </li>
-  )
+  );
 }
-
-export default MealItem
